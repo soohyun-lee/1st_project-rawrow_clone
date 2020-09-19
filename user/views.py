dafb6f0d36e9e6d37159bfd00a03bda176adee5d
soohyun-lee <soohyun527@gmail.com>
2020-09-19 15:27:22 +0900

    user models 셋팅

--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ import bcrypt
 from django.views        import View
 from django.http         import JsonResponse
 from .models             import User
-from my_settings         import SECRET, ALGORITHM
+from my_settings         import SECRET_KEY, ALGORITHM
 
 
 class SignUpView(View):
@@ -14,19 +14,26 @@ class SignUpView(View):
         password = data.get('password')
         password_len = 4
 
-        if not '@' in data['email'] or not '.' in data['email']:#이메일 형식이 안 맞을 때
-            return JsonResponse({'message':'Email_Form_Error'}, status=400)
-        
-        if len(str(password)) < password_len:#비밀번호가 4자리 이하일 때
-            return JsonResponse({'message':'Password_Form_Error'}, status=400)
-
-        if (User.objects.filter(name = data['userid']) or 
-            User.objects.filter(phone_num = data['phone_num']) or
-            User.objects.filter(email = data['email'])):
-            return JsonResponse({'message':'Already_in_use'}, status=400)
-
-        if ('email' or 'password' or 'userid' or 'address' or 'user_name' or 'phonenumber') not in data:
+        if ('email' not in data.keys() or
+            'password' not in data.keys() or
+            'userid' not in data.keys() or
+            'address' not in data.keys() or
+            'user_name' not in data.keys() or
+            'phonenumber' not in data.keys() or
+            'telephone' not in data.keys()):
             return JsonResponse({'message':'KEY_ERROR'},status=400)
+
+        if not '@' in data['email'] or not '.' in data['email']:#이메일 형식이 안 맞을 때
+            return JsonResponse({'message':'EMAIL_FORM_ERROR'}, status=400)
+
+        if len(str(password)) < password_len:#비밀번호가 4자리 이하일 때
+            return JsonResponse({'message':'PASSWORD_FORM_ERROR'}, status=400)
+
+        if (User.objects.filter(userid = data['userid']) or 
+            User.objects.filter(phonenumber = data['phonenumber']) or
+            User.objects.filter(email = data['email'])):
+            return JsonResponse({'message':'ALREADY_IN_USE'}, status=400)
+    
 
         password = bcrypt.hashpw(
             data['password'].encode('utf-8'),
@@ -34,16 +41,14 @@ class SignUpView(View):
         ).decode('utf-8')
 
         User(
-            user_name      = data['user_name'],
-            email     = data['email'],
-            password  = password,
-            userid    = data['userid'],
+            user_name   = data['user_name'],
+            email       = data['email'],
+            password    = password,
+            userid      = data['userid'],
             phonenumber = data['phonenumber'],
-            address = data['address'],
-            telephone = models.IntegerField()
-            email = models.CharField(max_length = 100)
-            sex = models.CharField(max_length=1, choices=(('M', '남자'), ('F', '여자')))
-            birthday = models.DateField(max_length=1, choices=(('S', '양력'), ('L', '음력')), null=True)
+            address     = data['address'],
+            telephone   = data['telephone']
+          
         ).save()
 
         return JsonResponse({'message':'SUCCESS'}, status=200)#그게 아니면 success 반환
@@ -56,11 +61,13 @@ class LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
         password = data.get('password', None)
+        #아이디 혹은 비밀번호를 빈칸으로 두고 로그인 하려고 했을 때,
+        if (data['userid'] == "" or
+            data['password'] == ""):
+            return JsonResponse({'message':'VALUES_ERROR'},status=400)
 
-        if ('userid' or 'password') not in data: #이메일, 비밀번호 값이 없을 때
-            return JsonResponse({'message':'KEY_ERROR'},status=400) #KEY_ERROR 반환
-        if User.objects.filter(userid=data['userid']):#입력한 이메일을 조회 후
-            login_user = User.objects.get(usrid=data['usrid'])#변수에 저장
+        if User.objects.filter(userid=data['userid']):#입력한 아이디를 조회 후
+            login_user = User.objects.get(userid=data['userid'])#변수에 저장
             if bcrypt.checkpw(password.encode('utf-8'), login_user.password.encode('utf-8')):
                 token = jwt.encode(
                     {
@@ -70,3 +77,6 @@ class LoginView(View):
                 return JsonResponse({'Authorization(SUCCESS)': token}, status=200)
             
             return JsonResponse({'message':'INVALID_USER'}, status=400)
+
+
+
