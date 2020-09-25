c66d9f5ca36b0af96fbf1fd5484e2468e8a801c8
soohyun-lee <soohyun527@gmail.com>
2020-09-25 11:18:06 +0900

    product 뷰2차 수정

--- Product/views.py
+++ Product/views.py
@@ -3,6 +3,7 @@ from django.views          import View
 from django.http                import JsonResponse
 from django.db.models.functions import Greatest
 from django.db.models import Max
+from django.db.models.functions import Coalesce
 
 from django.db             import models
 from .models               import (
@@ -30,24 +31,23 @@ class SearchProducts(View):
             'price'              : product.price,
             'sale_price'         : product.sale_price
             } for product in search_product]
-        
+
             return JsonResponse({'data':product_info}, status=200)
         return JsonResponse({'data':None}, status=200)
 
 class AllProducts(View):
     def get(self, request):
         order_method = int(request.GET.get('sort_method', None))
-        
+        orderbyList = ('-price','-sale_price')
+
         if order_method == 0:
             product_all  = Products.objects.order_by('?')
         elif order_method == 1:
-            product_all  = Products.objects.order_by('price')
+            product_all  = Products.objects.extra(select={"current_price":"COALESCE(sale_price, price)"}, order_by=["current_price"])
         elif order_method == 2:
             product_all  = Products.objects.order_by('-price')
-
-
         new_ = []
-        for product in product_all:        
+        for product in product_all:
             new_.append({
                 'sub_category_id'    : product.id,
                 'id'                 : product.id,
@@ -59,31 +59,78 @@ class AllProducts(View):
             })
 
         return JsonResponse({'data': new_}, status=200)
- 
+
 class CategoryProductList(View):
     def get(self, request):
         category_id = request.GET.get("category", None)
+        subcategory_id = request.GET.get("subcategory", None)
         order_method = int(request.GET.get('sort_method', None))
-
+        if order_method == 0:
+            order = '?'
+        elif order_method == 1:
+            order = 'price'
+        elif order_method == 2:
+            order = '-price'
+        
+        
+        
         if subcategory_id == None:
+            # subcategories = SubCategory.objects.filter(category_id = category_id).prefetch_related("products_set")
+            # if order_method == 1:
             subcategories = SubCategory.objects.filter(category_id = category_id).prefetch_related("products_set")
-        else:
-            subcategories = SubCategory.objects.filter(id = subcategory_id).prefetch_related("products_set") 
-               
-        new_ = []
-        for i in subcategories:
-            for product in Products.objects.filter(sub_category_id = i.id):        
-                new_.append({
-                    'sub_category_id'    : i.id,
-                    'id'                 : product.id,
-                    'name'               : product.name,
-                    'price'              : product.price,
-                    'thumbnail'          : product.thumbnail,
-                    'hover_image'        : product.hover_image,
-                    'sale_price'         : product.sale_price,
-                })
+            for i in subcategories:
+                new_= []
+                for product in Products.objects.filter(sub_category_id = i.id).order_by(order):
+                    new_.append({
+                        'sub_category_id'    : i.id,
+                        'id'                 : product.id,
+                        'name'               : product.name,
+                        'price'              : product.price,
+                        'thumbnail'          : product.thumbnail,
+                        'hover_image'        : product.hover_image,
+                        'sale_price'         : product.sale_price,
+                    })
+        
+            return JsonResponse({'data': new_}, status=200)
 
-        return JsonResponse({'data': new_}, status=200)
+
+        elif subcategory_id != None:
+            # subcategories = SubCategory.objects.filter(category_id = category_id).prefetch_related("products_set")
+            subcategories = SubCategory.objects.filter(id = subcategory_id).prefetch_related("products_set")
+            for i in subcategories:
+                new_= []
+                for product in Products.objects.filter(sub_category_id = i.id).order_by(order):
+                    new_.append({
+                        'sub_category_id'    : i.id,
+                        'id'                 : product.id,
+                        'name'               : product.name,
+                        'price'              : product.price,
+                        'thumbnail'          : product.thumbnail,
+                        'hover_image'        : product.hover_image,
+                        'sale_price'         : product.sale_price,
+                    })
+        
+            return JsonResponse({'data': new_}, status=200)
+
+
+
+
+        # else:
+        #     subcategories = SubCategory.objects.filter(id = subcategory_id).prefetch_related("products_set")
+
+        # new_ = []
+        #     for i in subcategories:
+        #         for product in Products.objects.filter(sub_category_id = i.id):
+        #             new_.append({
+        #                 'sub_category_id'    : i.id,
+        #                 'id'                 : product.id,
+        #                 'name'               : product.name,
+        #                 'price'              : product.price,
+        #                 'thumbnail'          : product.thumbnail,
+        #                 'hover_image'        : product.hover_image,
+        #                 'sale_price'         : product.sale_price,
+        #             })
+        #         return JsonResponse({'data': new_.order_by('-price')}, status=200)
 
 class ProductDetailView(View):
     def get(self, request, product_id):
@@ -125,6 +172,6 @@ class ProductDetailView(View):
                 }for i in related_id]
             })
 
-            return JsonResponse({"data": list(product_list)}, status = 200) 
+            return JsonResponse({"data": list(product_list)}, status = 200)
         except ValueError:
             return JsonResponse({"message": 'INVALID_PRODUCT'}, status = 400)
